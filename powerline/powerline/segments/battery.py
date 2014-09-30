edcca0ceabbdaa626670c3738d5fab184489e585
T.S. Foster <hi@tsf.io>
2014-09-30 14:53:35 +0100

    Update powerline

--- powerline/powerline/segments/battery.py
+++
@@ -1,5 +0,0 @@
-def battery(pl):
-  from subprocess import Popen, PIPE
-  p = Popen(['battery', '-o', 'tmux'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
-  output = p.stdout.read()
-  return [{'contents': output}]
