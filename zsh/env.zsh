a7221bd156c0946cc70991acb060d54207991d7b
T.S. Foster <hi@tsf.io>
2014-10-06 17:03:39 +0100

    Ensure `$EDITOR` is properly exported

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,7 @@
 VIM_CRONTAB=true
 DOTDIR=$HOME/.dotfiles/zsh
 GOPATH=$HOME/.gocode
-EDITOR=vim
+export EDITOR=vim
 PAGER="/bin/sh -c \"unset PAGER;col -b -x | vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' -c 'map <SPACE> <C-D>' -c 'map b <C-U>' -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\""
 
 HISTSIZE=1000
