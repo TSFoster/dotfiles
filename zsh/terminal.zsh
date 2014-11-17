127439ab05b7473cad6d391fa989e6386dafc990
T.S. Foster <hi@tsf.io>
2014-11-17 12:53:17 +0000

    Make zsh share history between instances

--- zsh/terminal.zsh
+++ zsh/terminal.zsh
@@ -5,24 +5,26 @@ TERM=xterm-256color-italic
 # Free up <C-s> <C-q> from flow control
 stty -ixon
 
-setopt AUTO_CD           # No need to type cd
-setopt MULTIOS           # Pipe to multiple outputs
-setopt CORRECT           # Spell check commands
-setopt AUTO_PUSHD        # pushd insted of cd
-setopt AUTO_NAME_DIRS    # Use named dirs when possible
-setopt PUSHDIGNOREDUPS   # Don't duplicate directories in stack
-setopt EXTENDED_GLOB     # GLOBBING!
-setopt GLOB_COMPLETE     # If we have a glob this will expand it
-setopt NULL_GLOB         # Match *.nomatches to nothing, not literal
-setopt PUSHD_SILENT      # No more annoying pushd messages
-setopt PUSHD_TO_HOME     # Blank pushd goes to home
-setopt PUSHD_IGNORE_DUPS # Ignore multiple directories for the stack
-setopt RM_STAR_WAIT      # 10 second wait before deleting everything
-setopt ZLE               # Magic
-setopt NO_HUP            # Don't kill background processes
-setopt VI && bindkey -v  # Vi mode
-setopt COMPLETEINWORD    # Tab completion from both ends
-setopt PROMPT_SUBST      # Prompt expansion
+setopt AUTO_CD            # No need to type cd
+setopt MULTIOS            # Pipe to multiple outputs
+setopt CORRECT            # Spell check commands
+setopt AUTO_PUSHD         # pushd insted of cd
+setopt AUTO_NAME_DIRS     # Use named dirs when possible
+setopt PUSHDIGNOREDUPS    # Don't duplicate directories in stack
+setopt EXTENDED_GLOB      # GLOBBING!
+setopt GLOB_COMPLETE      # If we have a glob this will expand it
+setopt NULL_GLOB          # Match *.nomatches to nothing, not literal
+setopt PUSHD_SILENT       # No more annoying pushd messages
+setopt PUSHD_TO_HOME      # Blank pushd goes to home
+setopt PUSHD_IGNORE_DUPS  # Ignore multiple directories for the stack
+setopt RM_STAR_WAIT       # 10 second wait before deleting everything
+setopt ZLE                # Magic
+setopt NO_HUP             # Don't kill background processes
+setopt VI && bindkey -v   # Vi mode
+setopt COMPLETEINWORD     # Tab completion from both ends
+setopt PROMPT_SUBST       # Prompt expansion
+setopt INC_APPEND_HISTORY # Save history as soon as command is executed
+setopt SHARE_HISTORY      # Share history
 
 # Keyboard Shortcuts
 bindkey '^O' clear-screen                                         # Clear screen shortcut
