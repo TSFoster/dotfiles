cc97493b029d3159a3008f35d186ce59f5385204
T.S. Foster <hi@tsf.io>
2015-02-27 23:03:50 +0000

    Remove git-icdiff

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -2,8 +2,8 @@ alias g='git'
 
 alias gst='git status'
 
-alias gd='git icdiff'
-alias gdc='git icdiff --cached'
+alias gd='git diff'
+alias gdc='git diff --cached'
 
 function ga { git add ${*:-.}; }
 
