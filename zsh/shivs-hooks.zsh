7dc6882b0b441e6e6afcfc85a5ba7412c11ff730
T.S. Foster <hi@tsf.io>
2016-02-24 10:24:24 +0100

    Add swiftenv

--- zsh/shivs-hooks.zsh
+++ zsh/shivs-hooks.zsh
@@ -1,4 +1,5 @@
 if exists direnv; then eval "$(direnv hook zsh)"; fi
 if exists pyenv; then eval "$(pyenv init -)"; fi
 if exists rbenv; then eval "$(rbenv init -)"; fi
+if exists swiftenv; then eval "$(swiftenv init -)"; fi
 export PATH
