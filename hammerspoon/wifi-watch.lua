92dd2b8cf3f0f4b7c420bb37b4bfbbcb51df238d
T.S. Foster <hi@tsf.io>
2016-03-03 12:36:01 +0100

    Fix wifi-watcher

--- hammerspoon/wifi-watch.lua
+++ hammerspoon/wifi-watch.lua
@@ -1,21 +1,21 @@
 local wifiWatcher = nil
 local homeSSID = "TRANEHAVEGAARD221DOR15"
 local lastSSID = hs.wifi.currentNetwork()
-local lastTime = 0
+local lastTime = os.time({year = 1979, month = 1, day = 1})
 
 function ssidChangedCallback()
   newSSID = hs.wifi.currentNetwork()
   device = hs.audiodevice.defaultOutputDevice()
-  thisTime = os.clock()
+  thisTime = os.time()
 
   if lastSSID == homeSSID then
-    lastTime = os.clock()
+    lastTime = os.time()
   end
 
   if newSSID == homeSSID and lastSSID ~= homeSSID and device:muted() then
-    device.setMuted(false)
+    device:setMuted(false)
     hs.alert.show("Volume unmuted, set to " .. device.volume)
-  elseif newSSID ~= homeSSID and lastSSID == homeSSID and (thisTime - lastTime) > (60 * 5) and not device.jackConnected() then
+  elseif newSSID ~= homeSSID and os.difftime(thisTime, lastTime) > 300 and not device:muted() then
     device:setMuted(true)
     hs.alert.show("Volume muted")
   end
