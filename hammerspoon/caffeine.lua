6912266caccde8779f1b9f1470eaa260f6afc24d
T.S. Foster <hi@tsf.io>
2016-04-07 10:58:21 +0200

    Add timer to caffeine.lua

--- hammerspoon/caffeine.lua
+++ hammerspoon/caffeine.lua
@@ -1,19 +1,86 @@
-local caffeine = hs.menubar.new()
+local menubar = nil
+local delayPeriod = hs.timer.minutes(10)
+local offSwitch = nil
+local menubarUpdater = nil
 
-function setCaffeineDisplay(state)
-  if state then
-    caffeine = hs.menubar.new()
-    caffeine:setTitle("👁")
-    caffeine:setClickCallback(caffeineClicked)
+local logger = hs.logger.new("CaffeineLogger", "debug")
+
+function setCaffeine(shouldCaffeinate)
+  if shouldCaffeinate then
+    resetTimerAndMenubar()
+    hs.caffeinate.set("displayIdle", true, true)
+    menubar = hs.menubar.new()
+    menubar:setClickCallback(turnOff)
+    updateMenubarTitle()
   else
-    caffeine:delete()
+    hs.caffeinate.set("displayIdle", false, false)
+    resetTimerAndMenubar()
   end
 end
 
-function caffeineClicked()
-  setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+
+function resetTimerAndMenubar()
+  if offSwitch ~= nil then
+    offSwitch:stop()
+    offSwitch = nil
+  end
+  if menubar ~= nil then
+    menubar:delete()
+    menubar = nil
+  end
+  if menubarUpdater ~= nil then
+    menubarUpdater:stop()
+    menubarUpdater = nil
+  end
 end
 
-setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 
-hs.hotkey.bind(hyper, "8", caffeineClicked)
+function turnOff()
+  setCaffeine(false)
+end
+
+
+function toggle()
+  if offSwitch ~= nil then
+    setCaffeine(true)
+  else
+    setCaffeine(not hs.caffeinate.get("displayIdle"))
+  end
+end
+
+
+function extend()
+  if menubar == nil then
+    setCaffeine(true)
+  end
+  if offSwitch == nil then
+    offSwitch = hs.timer.doAfter(delayPeriod, turnOff)
+  else
+    offSwitch:setNextTrigger(offSwitch:nextTrigger()+delayPeriod)
+  end
+  updateMenubarTitle()
+  menubarUpdater = hs.timer.doEvery(60, updateMenubarTitle)
+end
+
+
+function updateMenubarTitle()
+  if menubar ~= nil then
+    if offSwitch ~= nil then
+      minsLeft = math.ceil (offSwitch:nextTrigger()/60)
+      if minsLeft >= 60 then
+        remaining = math.floor (minsLeft/60) .. "h" .. (minsLeft % 60) .. "m"
+      else
+        remaining = minsLeft .. "m"
+      end
+    else
+      remaining = ""
+    end
+    menubar:setTitle("👁 " .. remaining)
+  end
+end
+
+
+setCaffeine(hs.caffeinate.get("displayIdle"))
+
+hs.hotkey.bind(hyper, "8", toggle)
+hs.hotkey.bind(hyper_cmd, "8", extend)
