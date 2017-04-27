6ba1547cf93e733e5eafa8761b9252bcd93fa851
T.S. Foster <hi@tsf.io>
2017-04-27 16:46:58 +0100

    Improve tab completion colours

--- fish/colors.fish
+++ fish/colors.fish
@@ -5,8 +5,12 @@ switch (cat ~/.lightdark)
         eval sh $shell_path.dark.sh
         set -e theme_color_scheme
         set -U theme_color_scheme terminal2-dark-white
+        set -e fish_pager_color_prefix
+        set -U fish_pager_color_prefix brblack
     case '*'
         eval sh $shell_path.light.sh
         set -e theme_color_scheme
         set -U theme_color_scheme terminal2-light-white
+        set -e fish_pager_color_prefix
+        set -U fish_pager_color_prefix white
 end
