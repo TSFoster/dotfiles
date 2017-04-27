d1f08247bce7a2d544cb8cd1a0db1b685c6cac6f
T.S. Foster <hi@tsf.io>
2017-04-27 11:20:12 +0100

    Add `tow` function

--- fish/aliases/git.fish
+++ fish/aliases/git.fish
@@ -2,3 +2,18 @@ alias git hub
 alias g git
 alias gst 'g status'
 alias gd 'g diff'
+
+if [ (type -t tower) = 'file' ]
+    function tow
+        if [ count $argv -eq 0 ]
+            tower $argv
+        else
+            set -l root (g root ^/dev/null)
+            if [ $root = '\n' ]
+                echo "You are not in a git repo" >&2
+            else
+                tower $root
+            end
+        end
+    end
+end
