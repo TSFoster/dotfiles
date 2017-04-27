4a7a872bd0d3222c4c35215be367484f3039f71f
T.S. Foster <hi@tsf.io>
2017-04-27 11:21:41 +0100

    Add `V` global alias

--- fish/functions/replace_then_execute.fish
+++ fish/functions/replace_then_execute.fish
@@ -2,6 +2,7 @@ function replace_then_execute
     set -l new_command ( \
         commandline \
             | sed 's/ N$/; terminal-notifier -message "Command finished"/g' \
+            | sed 's/ V$/ | v -/g' \
             | sed 's/ G / | ag -S /g' \
     )
     commandline -r $new_command
