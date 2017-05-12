75994ac52d2d31607a948b0209102b7984f771d0
T.S. Foster <hi@tsf.io>
2017-05-12 13:23:45 +0100

    Improve colorscheme

--- fish/colors.fish
+++ fish/colors.fish
@@ -1,16 +1,49 @@
 set -l shell_path $fish_dotfiles/colors/base16-strange_harmony
 
+set -e fish_pager_color_prefix
+set -e theme_color_scheme
+
 switch (cat ~/.lightdark)
     case 'Dark'
         eval sh $shell_path.dark.sh
-        set -e theme_color_scheme
-        set -U theme_color_scheme terminal2-dark-white
-        set -e fish_pager_color_prefix
         set -U fish_pager_color_prefix brblack
+        set colorfg black
+        set colorseg grey
+        set colorpath brgrey white
     case '*'
         eval sh $shell_path.light.sh
-        set -e theme_color_scheme
-        set -U theme_color_scheme terminal2-light-white
-        set -e fish_pager_color_prefix
         set -U fish_pager_color_prefix white
+        set colorfg brwhite
+        set colorseg brgrey
+        set colorpath grey black
 end
+
+set -U theme_color_scheme user
+
+set -U __color_initial_segment_exit     $colorseg red --bold
+set -U __color_initial_segment_su       $colorseg green --bold
+set -U __color_initial_segment_jobs     $colorseg blue --bold
+
+set -U __color_vagrant                  brcyan $colorfg
+set -U __color_rvm                      brmagenta $colorfg --bold
+set -U __color_virtualfish              brblue $colorfg --bold
+
+set -U __color_path_nowrite             magenta $colorfg
+set -U __color_path_nowrite_basename    magenta $colorfg --bold
+
+set -U __color_repo                     green $colorfg
+set -U __color_repo_work_tree           grey $colorfg --bold
+set -U __color_repo_dirty               brred $colorfg
+set -U __color_repo_staged              yellow $colorfg
+
+set -U __color_vi_mode_default          brblue $colorfg --bold
+set -U __color_vi_mode_insert           brgreen $colorfg --bold
+set -U __color_vi_mode_visual           bryellow $colorfg --bold
+
+set -U __color_path                     $colorpath
+set -U __color_path_basename            $colorpath --bold
+set -U __color_username                 $colorpath
+
+set -e colorfg
+set -e colorseg
+set -e colorpath
