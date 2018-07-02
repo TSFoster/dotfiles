6fa1c975b28d7d5a88af06f2082965bc4d88f320
T.S. Foster <hi@tsf.io>
2018-07-02 16:29:05 +0100

    Fix tower command when not in repo

--- omf/aliases/git.fish
+++ omf/aliases/git.fish
@@ -9,36 +9,37 @@ abbr -a gsp git stash pop
 abbr -a gca git commit --amend
 
 if [ (type -t gittower 2> /dev/null; or echo 'fail') = 'file' ]
-    function tower
-        if [ (count $argv) -gt 0 ]
-            command gittower $argv
-        else
-            set -l root (git root ^/dev/null)
-            if [ $root = '\n' ]
-                echo "You are not in a git repo" >&2
-            else
-                command gittower $root
-            end
-        end
+  function tower
+    if [ (count $argv) -gt 0 ]
+      command gittower $argv
+    else
+      set -l root (git root ^/dev/null)
+      if [ -z "$root" ]
+        echo "You are not in a git repo" >&2
+        exit 1
+      else
+        command gittower $root
+      end
     end
+  end
 end
 
 function ga
-    if [ (count $argv) -eq 0 ]
-        git add .
-    else
-        git add $argv
-    end
+  if [ (count $argv) -eq 0 ]
+    git add .
+  else
+    git add $argv
+  end
 end
 
 function gc
-    if [ (count $argv) -eq 0 ]
-        git commit
-    else
-        git commit -m $argv
-    end
+  if [ (count $argv) -eq 0 ]
+    git commit
+  else
+    git commit -m $argv
+  end
 end
 
 function rebase_last
-    git rebase -i HEAD~$argv
+  git rebase -i HEAD~$argv
 end
