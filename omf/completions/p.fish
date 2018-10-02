1cd398a2269bf2c40038e2db760fb3fc6004c36c
T.S. Foster <hi@tsf.io>
2018-10-02 17:20:23 +0100

    Update p functions for new environment variables

--- omf/completions/p.fish
+++ omf/completions/p.fish
@@ -1,24 +1,3 @@
-function __fish_p_get_project_path
-  if not set -q argv[1]
-    echo 'No project name given.' >&2
-    return 1
-  end
-  set projectName $argv[1]
-
-  if [ $projectName = home ]
-    echo $HOME
-  else if [ -f $projectsDir/$projectName.txt ]
-    cat $projectsDir/$projectName.txt
-  else if [ -d $workspace/$projectName ]
-    echo $workspace/$projectName
-  else
-    echo 'New directory must be created.' >&2
-    return 1
-  end
-
-  return 0
-end
-
 function __fish_p_at_stage
   set cmd (commandline -opc)
 
