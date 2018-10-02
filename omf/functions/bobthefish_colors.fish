0527a9ad28bdbe172126fd512dada48ed51a9b78
T.S. Foster <hi@tsf.io>
2018-10-02 21:46:14 +0100

    Export color variables correctly

--- omf/functions/bobthefish_colors.fish
+++ omf/functions/bobthefish_colors.fish
@@ -6,41 +6,41 @@ function bobthefish_colors -S -d 'Define a custom bobthefish color scheme'
   switch (cat $theme_file)
       case 'true'
           eval sh $shell_path.dark.sh
-          set -Ux fish_pager_color_prefix brblack
+          set -x fish_pager_color_prefix brblack
           set colorfg black
           set colorseg grey
           set colorpath brgrey white
       case '*'
           eval sh $shell_path.light.sh
-          set -Ux fish_pager_color_prefix white
+          set -x fish_pager_color_prefix white
           set colorfg brwhite
           set colorseg brgrey
           set colorpath grey black
   end
 
-  set -Ux color_initial_segment_exit     $colorseg red --bold
-  set -Ux color_initial_segment_su       $colorseg green --bold
-  set -Ux color_initial_segment_jobs     $colorseg blue --bold
+  set -x color_initial_segment_exit     $colorseg red --bold
+  set -x color_initial_segment_su       $colorseg green --bold
+  set -x color_initial_segment_jobs     $colorseg blue --bold
 
-  set -Ux color_vagrant                  brcyan $colorfg
-  set -Ux color_rvm                      brmagenta $colorfg --bold
-  set -Ux color_virtualfish              brblue $colorfg --bold
+  set -x color_vagrant                  brcyan $colorfg
+  set -x color_rvm                      brmagenta $colorfg --bold
+  set -x color_virtualfish              brblue $colorfg --bold
 
-  set -Ux color_path_nowrite             magenta $colorfg
-  set -Ux color_path_nowrite_basename    magenta $colorfg --bold
+  set -x color_path_nowrite             magenta $colorfg
+  set -x color_path_nowrite_basename    magenta $colorfg --bold
 
-  set -Ux color_repo                     green $colorfg
-  set -Ux color_repo_work_tree           grey $colorfg --bold
-  set -Ux color_repo_dirty               brred $colorfg
-  set -Ux color_repo_staged              yellow $colorfg
+  set -x color_repo                     green $colorfg
+  set -x color_repo_work_tree           grey $colorfg --bold
+  set -x color_repo_dirty               brred $colorfg
+  set -x color_repo_staged              yellow $colorfg
 
-  set -Ux color_vi_mode_default          brblue $colorfg --bold
-  set -Ux color_vi_mode_insert           brgreen $colorfg --bold
-  set -Ux color_vi_mode_visual           bryellow $colorfg --bold
+  set -x color_vi_mode_default          brblue $colorfg --bold
+  set -x color_vi_mode_insert           brgreen $colorfg --bold
+  set -x color_vi_mode_visual           bryellow $colorfg --bold
 
-  set -Ux color_path                     $colorpath
-  set -Ux color_path_basename            $colorpath --bold
-  set -Ux color_username                 $colorpath
+  set -x color_path                     $colorpath
+  set -x color_path_basename            $colorpath --bold
+  set -x color_username                 $colorpath
 
   set -e colorfg
   set -e colorseg
