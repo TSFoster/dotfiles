3f81a09f55d0e3386161f49038632b80f566584f
T.S. Foster <hi@tsf.io>
2020-02-17 12:15:43 +0000

    Update fish to 3.1

--- omf/ssh_agent.fish
+++ omf/ssh_agent.fish
@@ -4,8 +4,8 @@ command -s fkill > /dev/null; or exit
 command -s ssh-agent > /dev/null; or exit
 command -s ssh-add > /dev/null; or exit
 
-if [ -z "$SSH_AGENT_PID" -o -z "$SSH_AUTH_SOCK" ]; or echo (ssh-add -l ^&1) | grep -q "Error connecting to agent"
-    fkill ssh-agent ^ /dev/null
+if [ -z "$SSH_AGENT_PID" -o -z "$SSH_AUTH_SOCK" ]; or echo (ssh-add -l 2>&1) | grep -q "Error connecting to agent"
+    fkill ssh-agent 2> /dev/null
 
     set -q SSH_AGENT_PID; and set -e SSH_AGENT_PID
     set -q SSH_AUTH_SOCK; and set -e SSH_AUTH_SOCK
@@ -15,4 +15,4 @@ if [ -z "$SSH_AGENT_PID" -o -z "$SSH_AUTH_SOCK" ]; or echo (ssh-add -l ^&1) | gr
     chmod 600 $SSH_AUTH_SOCK
 end
 
-ssh-add -A ^ /dev/null
+ssh-add -A 2> /dev/null
