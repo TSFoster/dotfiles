e841db4225b8bb62cdae00563b23e8c7a999a1e0
T.S. Foster <hi@tsf.io>
2020-06-05 15:21:20 +0100

    Add viman completion (doesn’t really work on macOS though)

--- fish/functions/viman.fish
+++ fish/functions/viman.fish
@@ -1,4 +1,4 @@
-function viman --description 'Open manpage in new tab in nvim'
+function viman --wraps=man --description 'Open manpage in new tab in nvim'
   set location (man -w $argv[1])
   and nvr +"tabe +Man\\ $location|only"
 end
