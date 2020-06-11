ade3da3d3cb9ad5b2defa24cca4082e1ef9d48a6
T.S. Foster <hi@tsf.io>
2020-06-11 17:33:02 +0100

    Go through env in fish/conf.d/

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -1,5 +1,5 @@
-# TODO make many of these universal, set in a secript in meta/install/fish
-# TODO make some of these only if status --is-interactive?
+# TODO is it worth making most of these universal, set in a secript in meta/install/fish?
+
 set --export XDG_CONFIG_HOME $HOME/.config
 set --export XDG_DATA_HOME $HOME/.local/share
 set --global config $XDG_CONFIG_HOME
@@ -7,7 +7,8 @@ set --global data $HOME/.local/share
 set --global localBin $HOME/.local/bin
 set --global workspace $HOME/workspace
 
-set --export fish_key_bindings fish_vi_key_bindings
+# Set up go
+set --export GOPATH $XDG_DATA_HOME/go
 
 set --export fish_user_paths \
   /usr/local/bin \
@@ -16,11 +17,14 @@ set --export fish_user_paths \
   $localBin \
   ~/.cargo/bin \
   ./bin \
-  ./node_modules/.bin
+  ./node_modules/.bin \
+  $GOPATH/bin
+
+set --export fish_key_bindings fish_vi_key_bindings
 
 for dir in \
     /usr/local/opt/node@10/bin \
-    ~/.fzf/bin \
+    $HOME/.fzf/bin \
     /System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources \
     $HOME/Library/Python/3.7/bin \
     /Applications/Plex\ Media\ Server.app/Contents/MacOS
@@ -28,10 +32,11 @@ for dir in \
   and set --export fish_user_paths $fish_user_paths $dir
 end
 
-set --export SHELL (which fish)
 
-set --export OS Unknown
-uname | grep -q Darwin; and set --export OS Mac
+if not set --query OS
+  set --universal OS Unknown
+  uname | grep -q Darwin; and set --universal OS Mac
+end
 
 set --export PAGER less
 
@@ -51,20 +56,15 @@ end
 
 if [ $OS = Mac ]
   set --export HOMEBREW_CASK_OPTS --appdir=/Applications
-  # Put the airport command in PATH
   set --export iCloud "$HOME/Library/Mobile Documents/com~apple~CloudDocs/"
   set --export documents "$iCloud/Documents"
   set --export work "$documents/Work"
   set --export clients "$work/Clients"
 end
 
-set --export FD_DEFAULT_FLAGS '--exclude node_modules --exclude .git --exclude elm-stuff --exclude bower_components --exclude target --exclude .DS_Store --no-ignore-vcs --hidden'
+set --export FD_DEFAULT_FLAGS '--exclude node_modules --exclude .git --exclude elm-stuff --exclude bower_components --exclude target --exclude .DS_Store --exclude .sass-cache --no-ignore-vcs --hidden'
 set --export RIPGREP_CONFIG_PATH $config/ripgreprc
 set --export FZF_DEFAULT_COMMAND "fd --type file $FD_DEFAULT_FLAGS"
-
-# Set up go
-set --export GOPATH $XDG_DATA_HOME/go
-set --export fish_user_paths $GOPATH/bin $fish_user_paths
 
 # Set up Java
 set --export JAVA_HOME (/usr/libexec/java_home)
